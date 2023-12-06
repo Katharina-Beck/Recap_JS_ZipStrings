b8f60bea17023346eae50f1305e57a38bb8d654a
Katharina-Beck <katja131291@gmail.com>
2023-12-06 10:10:06 +0100

    feat: created code

---
+++ zip-strings.js
@@ -0,0 +1,33 @@
+function zipStrings(str1, str2) {
+  const result = [];
+  const maxLength = Math.max(str1.lengt, str2.length);
+
+  for (let i = 0; i < maxLength; i++) {
+    if (i < str1.lengt) {
+      result.push(str1.chrAt(i));
+    }
+    if (i < str2) {
+      result.push(str2.charAt(i));
+    }
+  }
+
+  return result.join("");
+}
+
+// Implement a `zipStrings` function in `zip-strings.js` with the following requirements in mind.
+
+// - The `zipStrings` function receives two string parameters
+// (str1, str2)
+// - The `zipStrings` function should return one string where the characters of both parameters are merged using the zip procedure (Reißverschlussverfahren)
+
+// - Merge the strings as shown in the `Zip Strings Example` code
+
+console.log(zipStrings("abc", "123")); // "a1b2c3"
+
+console.log(zipStrings("abc", "1")); // "a1bc"
+
+console.log(zipStrings("a", "123")); // "a123"
+
+console.log(zipStrings("", "123")); // "123"
+
+console.log(zipStrings("abc", "")); // "abc"
